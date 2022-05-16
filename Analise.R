433267acf23490a96918d8f3975506ba1522e937
augustocfg <augustocesarfg#gmail.com>
2022-05-16 00:21:15 -0300

    Adicionando mais gráficos e tabelas.

--- Analise.R
+++ Analise.R
@@ -5,7 +5,11 @@ library(zoo)
 library(ggplot2)
 library(scales)
 library(lubridate)
+library(ggThemeAssist)
 library(formattable)
+library(reactable)
+library(esquisse)
+library(reactablefmtr)
 
 X20220406_sigdef <- read_excel("20220406_sigdef.xls", 
                                sheet = "arrecadacao")
@@ -27,14 +31,24 @@ RN_FISCAL %>%
          OUTROS,
          `TOTAL GERAL DA RECEITA TRIBUTÁRIA`) %>%
   rename(ICMS = `TOTAL DA ARRECADAÇÃO DO ICMS`) %>%
-  pivot_longer(cols = c(ICMS, IPVA, ITCD, OUTROS),
+  pivot_longer(cols = c(ICMS, IPVA, ITCD, OUTROS,
+                        `TOTAL GERAL DA RECEITA TRIBUTÁRIA`),
                names_to = "Tributos",
-               values_to = "Montante") -> Impostos_Totais
+               values_to = "Montante") %>% 
+  arrange(Período, desc(Montante)) %>% 
+  mutate(Tributos = factor(Tributos,
+                           levels = c("TOTAL GERAL DA RECEITA TRIBUTÁRIA",
+                                      "ICMS",
+                                      "IPVA",
+                                      "ITCD",
+                                      "OUTROS")))-> Impostos_Totais
 
 
 StyleTheme <-  theme_minimal() +
   theme(panel.grid.minor = element_blank(),
-        axis.title.x  = element_blank())
+        axis.title.x  = element_blank()) 
+
+#Conteúdo 1
 
 
 Impostos_Totais %>%
@@ -43,31 +57,84 @@ Impostos_Totais %>%
   scale_y_continuous(labels = comma_format(big.mark = ".",
                                            decimal.mark = ",")) +
   StyleTheme +
-  ggtitle("Arrecadação de Impostos do RN e Suas Evoluções (1997 - 2022)")
+  ggtitle("Arrecadação de Impostos do RN e Suas Evoluções (1997 - 2022)") + 
+  scale_colour_manual(values = c("#32373b", "#62b851",
+                                 "#009fb7", "#b20d30",
+                                 "#f4b942"))
 
-
-
-Impostos_Totais %>% 
-  filter(Período > "dez 2021") 
+#Conteúdo 2
   
 
 Impostos_Totais %>%
-  filter(Período > "dez 2014") %>%
+  filter(Período > "dez 2014",
+         Tributos != "OUTROS") %>%
   mutate(Ano = year(Período)) %>%
-  group_by(Ano, Tributos) %>%
-  summarise(Montante = sum(Montante),
-            Total = sum(`TOTAL GERAL DA RECEITA TRIBUTÁRIA`)) %>%
-  mutate(prop = round(Montante/Total, digits = 2)) %>%
-  filter(Tributos != "OUTROS") %>%
-  ggplot(., aes(x = Ano, y = Montante, label = prop)) +
+  pivot_wider(names_from = Tributos, values_from = Montante) %>% 
+  group_by(Ano) %>%
+  summarise(ICMS = sum(ICMS),
+            IPVA = sum(IPVA),
+            ITCD= sum(ITCD),
+            Total = sum(`TOTAL GERAL DA RECEITA TRIBUTÁRIA`)) %>% 
+  pivot_longer(-c(Total, Ano),
+               names_to = "Tributos",
+               values_to = "Montante") %>% 
+  mutate(prop = (Montante/Total)) %>%
+  select(-c(Montante, Total)) %>% 
+  pivot_wider(names_from = Tributos, values_from = prop)  %>% 
+  reactable(defaultColDef = colDef(headerClass = "header", align = "center",
+                                   headerStyle = list(
+                                     background = "#7290ba",
+                                     color  = "#f5f5f5")),
+            columns = list(
+              Ano = colDef(align = "left"),
+              ICMS = colDef(
+                cell = data_bars(., 
+                                 number_fmt = formattable::percent,
+                                 text_position = "outside-base",
+                                 force_outside = c(0,0.4),
+                                 fill_color = "#7290ba")
+              ),
+              IPVA = colDef(
+                cell = data_bars(., 
+                                 number_fmt = formattable::percent,
+                                 text_position = "outside-base",
+                                 force_outside = c(0,0.4),
+                                 fill_color = "#7290ba")
+              ),
+              ITCD = colDef(
+                cell = data_bars(., 
+                                 number_fmt = formattable::percent,
+                                 text_position = "outside-base",
+                                 force_outside = c(0,0.4),
+                                 fill_color = "#7290ba")
+              )
+  ),
+  bordered = T,
+  striped = T
+  ) %>% 
+  google_font("Roboto")
+
+
+#Conteúdo 3
+
+RN_FISCAL %>% 
+  filter(Período > "dez 2020") %>% 
+  select(Período:`1.3 - TERCIÁRIO`, `TOTAL DA ARRECADAÇÃO DO ICMS`) -> SetoresI
+
+SetoresI %>% 
+  pivot_longer(cols = -c(Período, `TOTAL DA ARRECADAÇÃO DO ICMS`), 
+               names_to = "Setores",
+               values_to = "Montante") %>% 
+  mutate(Setores = substring(Setores, first = 7)) %>% 
+  ggplot(aes(Período, Montante, fill = Setores)) +
+  geom_col(position = "stack") +
   StyleTheme +
-  geom_col(position = "stack",aes(fill = Tributos)) +
-  geom_text(hjust = "center")
+  scale_fill_manual(values = c("#62b851", "#32373b",
+                                 "#009fb7", "#b20d30",
+                                 "#f4b942")) +
+  scale_y_continuous(labels = comma_format(big.mark = ".",
+                                           decimal.mark = ",")) +
+  ggtitle("Valor arrecadado do ICMS por setores")
 
-
-
-  
-
-
-
+#Conteúdo 4
 
